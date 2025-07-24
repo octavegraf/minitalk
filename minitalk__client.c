24a545e4d0b5a140f387deb8b1bcc2fdcaa59b9c
Octave Graf <ocgraf@student.42.fr>
2025-07-24 14:36:52 +0200

    stronger verifications and improved logic

--- minitalk__client.c
+++ minitalk__client.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 14:25:04 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/23 17:28:47 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/24 14:32:41 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,45 @@ int	send_str(int pid, char c)
 	bit = 0;
 	while (bit < 8)
 	{
-		if ((c >> (7 - bit)) & 1)
-		{	
-			if (kill(pid, SIGUSR1))
+		if ((c & (1 << (7 - bit))))
+		{
+			if (kill(pid, SIGUSR1) == -1)
 				return (-1);
 		}
 		else
-			if (kill(pid, SIGUSR2))
+		{
+			if (kill(pid, SIGUSR2) == -1)
 				return (-1);
+		}
 		bit++;
 		usleep(DELAY);
 	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	pid;
+	int	buff_len;
 
-	if (argc != 2)
+	if (argc != 3)
 		return (1);
 	i = 0;
-	pid = atoi(argv[1]);
-	while (argv[2][i++])
+	buff_len = 0;
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i])
 	{
-		if (send_str(pid, argv[1][i]) == -1)
-			return (1);
-		usleep(DELAY);
+		if (buff_len == BUFFER - 1)
+		{
+			usleep(DELAY * 1.5);
+			buff_len = 0;
+		}
+		if (send_str(pid, argv[2][i]) == -1)
+			return (ft_putstr("Server error."), 1);
+		i++;
+		buff_len++;
 	}
+	send_str(pid, '\0');
 	return (0);
 }
--- minitalk__server.c
+++ minitalk__server.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 12:34:43 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/23 17:28:26 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/24 14:32:52 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,38 @@
 
 t_msg	g_msg;
 
-void	msg_reset(void)
+void	reset_msg(void)
 {
-	ft_bzero(g_msg.buffer, BUFFER * sizeof(char));
-	g_msg.buff_len = 0;
 	g_msg.bit = 0;
+	g_msg.len = 0;
+	ft_bzero(g_msg.buffer, BUFFER);
 }
 
 void	sighandler(int sig)
 {
-	if (g_msg.buff_len == BUFFER - 1)
+	if (g_msg.len == BUFFER - 1)
 	{
 		ft_printf("%s", g_msg.buffer);
-		msg_reset();
+		reset_msg();
+
 	}
 	if (g_msg.bit < 8)
 	{
-		g_msg.buffer[g_msg.buff_len] = (g_msg.buffer[g_msg.buff_len] << 1);
+		g_msg.buffer[g_msg.len] = (g_msg.buffer[g_msg.len] << 1);
 		if (sig == SIGUSR1)
-			g_msg.buffer[g_msg.buff_len] |= 1;
+			g_msg.buffer[g_msg.len] |= 1;
 		g_msg.bit++;
-	}
-	else
-	{
-		g_msg.bit = 0;
-		if (g_msg.buffer[g_msg.buff_len] == '\0')
+		if (g_msg.bit == 8)
 		{
-			ft_printf("%s\n", g_msg.buffer);
-			msg_reset();
+			g_msg.bit = 0;
+			if (!g_msg.buffer[g_msg.len])
+			{
+				ft_printf("%s\n", g_msg.buffer);
+				reset_msg();
+			}
+			else
+				g_msg.len++;
 		}
-		else
-			g_msg.buff_len++;
 	}
 }
 
