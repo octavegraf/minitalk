6017380d777fc558efc5a5516ea3d0382c6b4000
Octave Graf <ocgraf@student.42.fr>
2025-07-25 15:18:54 +0200

    better delay / buffer

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 13:57:12 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/25 14:16:55 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/25 14:51:05 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # include <stdlib.h>
 # include "libft/libft.h"
 
-# define BUFFER 1024
-# define DELAY 60
+# define BUFFER 131072
+# define DELAY 100
 
 typedef struct s_msg
 {
