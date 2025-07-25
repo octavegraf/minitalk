e70580aa676c5975a2e8b7f1b88a70f171cfae10
Octave Graf <ocgraf@student.42.fr>
2025-07-25 14:12:10 +0200

    removed verifications that takes too much time

--- minitalk__server.c
+++ minitalk__server.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 12:34:43 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/24 14:32:52 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/25 13:42:59 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,29 +23,28 @@ void	reset_msg(void)
 
 void	sighandler(int sig)
 {
-	if (g_msg.len == BUFFER - 1)
+	g_msg.buffer[g_msg.len] <<= 1;
+	if (sig == SIGUSR1)
+		g_msg.buffer[g_msg.len] |= 1;
+	g_msg.bit++;
+	if (g_msg.bit == 8)
 	{
-		ft_printf("%s", g_msg.buffer);
-		reset_msg();
-
-	}
-	if (g_msg.bit < 8)
-	{
-		g_msg.buffer[g_msg.len] = (g_msg.buffer[g_msg.len] << 1);
-		if (sig == SIGUSR1)
-			g_msg.buffer[g_msg.len] |= 1;
-		g_msg.bit++;
-		if (g_msg.bit == 8)
+		if (g_msg.buffer[g_msg.len] == '\0')
 		{
-			g_msg.bit = 0;
-			if (!g_msg.buffer[g_msg.len])
+			write(1, g_msg.buffer, g_msg.len);
+			write(1, "\n", 1);
+			reset_msg();
+		}
+		else
+		{
+			g_msg.len++;
+			if (g_msg.len >= BUFFER - 1)
 			{
-				ft_printf("%s\n", g_msg.buffer);
+				write(1, g_msg.buffer, g_msg.len);
 				reset_msg();
 			}
-			else
-				g_msg.len++;
 		}
+		g_msg.bit = 0;
 	}
 }
 
@@ -58,8 +57,6 @@ int	main(void)
 	signal(SIGUSR1, sighandler);
 	signal(SIGUSR2, sighandler);
 	while (1)
-	{
 		pause();
-	}
 	return (0);
 }
