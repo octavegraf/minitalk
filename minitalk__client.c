aba471d2b210e245c51037314ede600db1795e62
Octave Graf <ocgraf@student.42.fr>
2025-07-25 13:57:06 +0200

    removed useless modifications because of the server optimisation

--- minitalk__client.c
+++ minitalk__client.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 14:25:04 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/07/24 14:32:41 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/25 13:54:04 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,16 @@ int	main(int argc, char **argv)
 {
 	int	i;
 	int	pid;
-	int	buff_len;
 
 	if (argc != 3)
 		return (1);
 	i = 0;
-	buff_len = 0;
 	pid = ft_atoi(argv[1]);
 	while (argv[2][i])
 	{
-		if (buff_len == BUFFER - 1)
-		{
-			usleep(DELAY * 1.5);
-			buff_len = 0;
-		}
 		if (send_str(pid, argv[2][i]) == -1)
 			return (ft_putstr("Server error."), 1);
 		i++;
-		buff_len++;
 	}
 	send_str(pid, '\0');
 	return (0);
